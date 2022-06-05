fbf4185dd20ca787ab91303c1eaafd5020144bfc
Forever Young <dy9040@gmail.com>
2022-06-05 13:55:34 -0500

    add user and route

--- models/Post.js
+++ models/Post.js
@@ -11,18 +11,27 @@ Post.init(
             primaryKey: true,
             autoIncrement: true
         },
-        post_text: {
+        text: {
             type: DataTypes.STRING,
             allowNull: false
         },
         post_url: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isUrl: true
         }
     },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    }
+},
     {
         sequelize,
-        timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'post',
