1cdc27226cc303e945f2e6ff1dc84ed0e5c19eda
Forever Young <dy9040@gmail.com>
2022-06-05 20:33:54 -0500

    more handle bars got a home page working

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-const hbs = exphbs.create({});
-
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const shush = {
+secret: 'shush... secret',
+cookie: {},
+resave: false,
+saveUninitialized: true,
+store: new SequelizeStore({
+    db: sequelize
+})
+};
+
+app.use(session(shush));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(require('./controllers/'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => 
